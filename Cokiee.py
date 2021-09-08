1639d1a6d600b204bb36a3a8315fc5b07c8f9439
MrxMilzu <89538890+MrxMilzu@users.noreply.github.com>
2021-09-08 13:08:35 +0700

    Update Cokiee.py

--- Cokiee.py
+++ Cokiee.py
@@ -20,9 +20,9 @@ def gopal(s):
 def adudu():
     print("""
         ╔═╗╔═╗╦ ╦ ╦ ╔═╗ ╔═╗╔╗      
-           ║   ║  ║║╣  ║ ║_   ║_  ╠╩╗           
+        ║   ║  ║║╣  ║ ║_   ║_  ╠╩╗           
         ║   ║  ║║║  ║ ║    ║   ║  ║
-          ╚═╝╚═╝╩ ╩ ╩ ╚═╝ ╝   ╚═╝  V.1 """)
+        ╚═╝╚═╝╩ ╩ ╩ ╚═╝ ╝   ╚═╝  V.1 """)
 
 def fank():
     a=requests.get("http://ip-api.com/json/",headers={"Referer":"http://ip-api.com/","Content-Type":"application/json; charset=utf-8","userAgent":"Mozilla/5.0 (Linux; Android 6.0.1; SM-G532G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.39 Mobile Safari/537.36"}).json()
