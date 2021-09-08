918608991e5b920726cbf862a617faa85204b55c
MrxMilzu <89538890+MrxMilzu@users.noreply.github.com>
2021-09-08 13:20:29 +0700

    Update Cokiee.py

--- Cokiee.py
+++ Cokiee.py
@@ -21,8 +21,8 @@ def adudu():
     print("""
         ╔═╗╔═╗╦ ╦ ╦ ╔═╗ ╔═╗╔╗      
         ║  ║ ║║╣  ║ ║_  ║_ ╠╩╗           
-        ║  ║ ║║║  ║ ║   ║  ║  ║
-        ╚═╝╚═╝╩ ╩ ╩ ╚═╝ ╝  ╚═╝  V.1 """)
+        ║  ║ ║║║  ║ ║   ║  ║ ║
+        ╚═╝╚═╝╩ ╩ ╩ ╚═╝ ╝  ╚═╝  By.Milzu-TC """)
 
 def fank():
     a=requests.get("http://ip-api.com/json/",headers={"Referer":"http://ip-api.com/","Content-Type":"application/json; charset=utf-8","userAgent":"Mozilla/5.0 (Linux; Android 6.0.1; SM-G532G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.39 Mobile Safari/537.36"}).json()
